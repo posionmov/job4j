a30c3a465587846f94e6750df77305923e1b93ee
posionmov <mailto:posionmov@gmail.com>
2018-07-19 00:12:53 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -7,12 +7,23 @@
 
 package ru.job4j.calculate;
 
-/**
-* Main
-* @param args - args.
-*/
+
 public class Calculate {
+	
+	/**
+	* Main
+	* @param args - args.
+	*/	
 	public static void main (String[] args) {
 		System.out.println("Hello world!");
+	}
+	
+	/**
+	* Method echo
+	* @param name - Your name
+	* @return Echo plus your name
+	*/
+	public String echo(String name) {
+		return "Echo, Echo, Echo : " + name;
 	}
 }
