cd500654e2622d211793cff9023df312f21676aa
posionmov <mailto:posionmov@gmail.com>
2018-07-18 23:30:31 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,16 @@
+/**
+* Calculate.
+* @author Sergey Galanov
+* @since 18.07.2018.
+* @version 1.0.
+*/
+
 package ru.job4j.calculate;
 
+/**
+* Main
+* @param args - args.
+*/
 public class Calculate {
 	public static void main (String[] args) {
 		System.out.println("Hello world!");
