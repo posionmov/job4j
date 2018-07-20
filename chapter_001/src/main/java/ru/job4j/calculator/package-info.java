8690c7099bd04646af7cb3532a48f6d1ad83d676
posionmov <mailto:posionmov@gmail.com>
2018-07-20 17:01:51 +0300

    3.1. Элементарный калькулятор. [#185]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,9 @@
+package ru.job4j.calculator;
+
+/**
+ * Package for calculator task.
+ *
+ * @author Sergey Galanov
+ * @version 1.0
+ * @since 20.07.2018
+ */
---
+++ chapter_001/src/test/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,9 @@
+package ru.job4j.calculator;
+
+/**
+ * Package for testing result for calculator task.
+ *
+ * @author Sergey Galanov
+ * @version 1.0
+ * @since 20.07.2018
+ */
