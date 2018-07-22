8c4089a7d72995455a0b4d5ab3149927ace8a68b
posionmov <mailto:posionmov@gmail.com>
2018-07-22 15:18:49 +0300

    5.2. Создать программу вычисляющую факториал. [#193]

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -16,12 +16,12 @@ public class Factorial {
     public int calc(int n) {
         int result = 1;
         if (n == 0) {
-            return 1;
+            result = 1;
         } else {
             for (int i = 1; i <= n; i++) {
                 result *= i;
             }
-            return result;
         }
+        return result;
     }
 }
