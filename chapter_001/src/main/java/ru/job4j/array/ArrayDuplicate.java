7a3dbb1a11afc15cbbfff73e23535c3c0aed7397
posionmov <mailto:posionmov@gmail.com>
2018-07-26 12:41:16 +0300

    6.8. Удаление дубликатов в массиве. [#225][#62695]

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -15,12 +15,13 @@ public class ArrayDuplicate {
      * @param array
      * @return sorted array without duplicates
      */
+    /*
     public String[] remove(String[] array) {
         int iterations = 0;
         String tmp;
         for (int i = 0; i != array.length - 2; i++) {
             for (int j = i + 1; j != array.length - 1; j++) {
-                if (array[j] == array[i]) {
+                if (array[j].equals(array[i])) {
                     tmp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = tmp;
@@ -29,5 +30,47 @@ public class ArrayDuplicate {
             }
         }
         return Arrays.copyOf(array, iterations - 1);
+    }
+    */
+    public String[] removing(String[] array) {
+        String tmp;
+        int iter = 0;
+        for (int i = 0; i != array.length - 1; i++) {
+            for (int j = i + 1; j != array.length; j++) {
+                if (!array[j].equals("forRemoving")) {
+                    if (array[i].equals(array[j])) {
+                        array[j] = "forRemoving";
+                    }
+                }
+                if (j == array.length - 1 && !array[j].equals("forRemoving")) {
+                    for (int index = j - 1; index > 0; index--) {
+                        if (array[index].equals("forRemoving")) {
+                            tmp = array[index];
+                            array[index] = array[j];
+                            array[j] = tmp;
+                            break;
+                        }
+                    }
+                }
+            }
+
+        }
+        for (int i = 0; i != array.length; i++) {
+            if (!array[i].equals("forRemoving")) {
+                iter++;
+            }
+            if (array[i].equals("forRemoving")) {
+                for (int j = i + 1; j != array.length; j++) {
+                    if (!array[j].equals("forRemoving")) {
+                        tmp = array[i];
+                        array[i] = array[j];
+                        array[j] = tmp;
+                        iter++;
+                        break;
+                    }
+                }
+            }
+        }
+        return Arrays.copyOf(array, iter);
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -18,9 +18,27 @@ public class ArrayDuplicateTest {
     @Test
     public void whenArrayWithDuplicatesThenArrayWithoutDuplicates() {
         ArrayDuplicate arrayDuplicate = new ArrayDuplicate();
-        String[] array = {"Привет", "Мир", "Привет", "Супер", "asd", "Мир", "qwe"};
-        String[] result = arrayDuplicate.remove(array);
+        String[] array = {"Привет", "Мир", "Привет", "Супер", "asd", "Привет", "Мир", "qwe"};
+        String[] result = arrayDuplicate.removing(array);
         String[] expect = {"Привет", "Мир", "Супер", "asd", "qwe"};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayWithTwoDuplicatesThenArrayWithoutDuplicates() {
+        ArrayDuplicate arrayDuplicate = new ArrayDuplicate();
+        String[] array = {"Привет", "Мир", "Супер", "Привет", "asd", "Привет", "Привет"};
+        String[] result = arrayDuplicate.removing(array);
+        String[] expect = {"Привет", "Мир", "Супер", "asd"};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayAllOneThenOneItem() {
+        ArrayDuplicate arrayDuplicate = new ArrayDuplicate();
+        String[] array = {"Привет", "Привет", "Привет", "Привет", "Привет"};
+        String[] result = arrayDuplicate.removing(array);
+        String[] expect = {"Привет"};
         assertThat(result, is(expect));
     }
 }
