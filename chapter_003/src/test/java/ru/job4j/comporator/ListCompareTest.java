85661dd3c5df35d90e017c7553cff98590a88ce1
posionmov <mailto:posionmov@gmail.com>
2018-08-07 14:15:04 +0300

    1. JaCoCo. Процент покрытие тестами. [#31726]

--- chapter_003/src/test/java/ru/job4j/comporator/ListCompareTest.java
+++ chapter_003/src/test/java/ru/job4j/comporator/ListCompareTest.java
@@ -18,7 +18,7 @@ public class ListCompareTest {
      * Тест при условии что обе строки равны
      */
     @Test
-    public void whenStringsAreEqualThenZero () {
+    public void whenStringsAreEqualThenZero() {
         ListCompare compare = new ListCompare();
         int rst = compare.compare(
                 "Ivanov",
@@ -31,7 +31,7 @@ public class ListCompareTest {
      * Тест при условии, что первая строка меньше второй
      */
     @Test
-    public void whenLeftLessThanRightResultShouldBeNegative () {
+    public void whenLeftLessThanRightResultShouldBeNegative() {
         ListCompare compare = new ListCompare();
         int rst = compare.compare(
                 "Ivanov",
@@ -44,7 +44,7 @@ public class ListCompareTest {
      * Тест при условии что первая строка больше второй
      */
     @Test
-    public void whenLeftGreaterThanRightResultShouldBePositive () {
+    public void whenLeftGreaterThanRightResultShouldBePositive() {
         ListCompare compare = new ListCompare();
         int rst = compare.compare(
                 "Petrov",
@@ -57,7 +57,7 @@ public class ListCompareTest {
      * Тест при условии, что в первой строке единственное отличие - это более большая буква (в списе букв)
      */
     @Test
-    public void secondCharOfLeftGreaterThanRightShouldBePositive(){
+    public void secondCharOfLeftGreaterThanRightShouldBePositive() {
         ListCompare compare = new ListCompare();
         int rst = compare.compare(
                 "Petrov",
@@ -70,7 +70,7 @@ public class ListCompareTest {
      * Тест при условии, что одна буква в левой строке меньше, чем буква во второй строке
      */
     @Test
-    public void secondCharOfLeftLessThanRightShouldBeNegative(){
+    public void secondCharOfLeftLessThanRightShouldBeNegative() {
         ListCompare compare = new ListCompare();
         int rst = compare.compare(
                 "Patrova",
