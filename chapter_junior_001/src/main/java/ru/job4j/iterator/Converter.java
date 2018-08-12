ab57b954193a4cac9a6afd18c11774305b784927
posionmov <mailto:posionmov@gmail.com>
2018-08-12 20:22:48 +0300

    5.1.4. Создать convert(Iterator<Iterator>) [#152]

--- chapter_junior_001/src/main/java/ru/job4j/iterator/Converter.java
+++ chapter_junior_001/src/main/java/ru/job4j/iterator/Converter.java
@@ -3,32 +3,65 @@ package ru.job4j.iterator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * Класс для конвертации нескольких последовательностей в одну
+ * @author Galanov Sergey
+ * @since 12.08.2018
+ * @version 1.0
+ */
 public class Converter {
 
     Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
 
+        /**
+         * Анонимный класс иетратора итераторов
+         */
         return new Iterator<Integer>() {
 
-            Iterator<Integer> iterator = it.next();
+            /**
+             * Содержит внутреннее поле с ссылкой на текущий итератор
+             */
+            Iterator<Integer> curIterator = it.next();
 
-
+            /**
+             * Реализация метода интерфейса
+             * @return true - если у за текущим элементов есть еще один элемент
+             */
             @Override
             public boolean hasNext() {
-                return iterator != null;
+                return find();
             }
 
+            /**
+             * Реализация метода интерфейса
+             * @return значение из текущего итератора и передвигает указатель дальше
+             */
             @Override
             public Integer next() {
-                if (!iterator.hasNext()) {
+                Integer result;
+                if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
-                Integer result = iterator.next();
-                if (!iterator.hasNext()) {
-                    iterator = it.hasNext() ? it.next() : null;
-                }
-
+                result = curIterator.next();
                 return result;
             }
+
+            /**
+             * Вспомогательный метод
+             * Производит корректное сдвжение указателя, а также возращает true если далее есть еще элементы
+             * @return true если далее есть еще элементы
+             */
+            public boolean find() {
+                boolean isExist = false;
+                if (!curIterator.hasNext() && it.hasNext()) {
+                    curIterator = it.next();
+                }
+                if (curIterator.hasNext()) {
+                   isExist = true;
+                }
+                return isExist;
+            }
+
         };
     }
 
