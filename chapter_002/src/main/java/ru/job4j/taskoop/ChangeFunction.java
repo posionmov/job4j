2eb8d8fe2781eab4e6ced165f0631b74939d16d4
posionmov <mailto:posionmov@gmail.com>
2018-08-14 18:09:13 +0300

    Проверочное задание по теме ООП[#64751]

--- chapter_002/src/main/java/ru/job4j/taskoop/ChangeFunction.java
+++ chapter_002/src/main/java/ru/job4j/taskoop/ChangeFunction.java
@@ -4,8 +4,13 @@ import java.util.Arrays;
 
 /**
  * Класс, содержащий метод выдачи сдачи в зависимости от внесенных денег
+ * @author Galanov Sergey
+ * @since 14.08.2018
+ * @version 1.3
  */
 public class ChangeFunction {
+
+    private int[] curMoney = new int[] {1, 2, 5, 10}; // Список типов всех монет
 
     /**
      * Метод выдачи сдачи в зависимости от внесенных денег
@@ -14,20 +19,30 @@ public class ChangeFunction {
      * @return массив, содержащий все монеты, которые должны пойти на сдачу
      */
     public int[] change(int value, int price) {
-        int[] result = new int[100];
-        int[] curMoney = new int[] {1, 2, 5, 10};
+        int[] result = new int[0];
         int resultSum = value - price;
         String answer = value >= price ? String.format("Ваша сдача: %s", resultSum) : String.format("Вам не хватает: %s", 0 - resultSum);
         System.out.println(answer);
-        int valueInArray = 0;
         if (value > price) {
-            for (int i = curMoney.length - 1; i >= 0; i--) {
-                if (resultSum >= curMoney[i]) {
-                    for (int iter = 0; iter != (resultSum / curMoney[i]); iter++) {
-                        result[valueInArray++] = curMoney[i];
-                    }
-                    resultSum -= curMoney[i] * (resultSum / curMoney[i]);
+            result = this.calculateChange(resultSum);
+        }
+        return result;
+    }
+
+    /**
+     * Метод подсчета сдачи
+     * @param sumForChange - Сумма, которую требуется дать в сдаче
+     * @return массив монет, которые наиболее рационально могут дать сдачи
+     */
+    private int[] calculateChange(int sumForChange) {
+        int[] result = new int[100];
+        int valueInArray = 0;
+        for (int i = this.curMoney.length - 1; i >= 0; i--) {
+            if (sumForChange >= this.curMoney[i]) {
+                for (int iter = 0; iter != (sumForChange / this.curMoney[i]); iter++) {
+                    result[valueInArray++] = this.curMoney[i];
                 }
+                sumForChange -= this.curMoney[i] * (sumForChange / this.curMoney[i]);
             }
         }
         return Arrays.copyOf(result, valueInArray);
