92cbf760c463142b2b3ca5ea7b1f2a094c08d50c
posionmov <mailto:posionmov@gmail.com>
2018-08-29 11:17:56 +0300

    1. Реализовать шаблон Producer Consumer. [#1098][#67992]

--- chapter_junior_002_multi_tasking/src/test/java/ru/job4j/notify/SimplyBlockingQueneTest.java
+++ chapter_junior_002_multi_tasking/src/test/java/ru/job4j/notify/SimplyBlockingQueneTest.java
@@ -19,24 +19,46 @@ public class SimplyBlockingQueneTest {
     private int value;
 
     /**
-     * Тест на изменение состояния потока
+     * Тест на изменение состояния потока если первым запустится потребитель
      * @throws InterruptedException если поток будет прерван при выполнении операции
      */
     @Test
     public void whenQueneIsEmptyThenFirstWorkIsOffer() throws InterruptedException {
         SimplyBlockingQuene<Integer> queue = new SimplyBlockingQuene<>();
-        Thread poll = new Thread(() -> {
+        Thread consumer = new Thread(() -> {
             try {
                 value = queue.poll();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         });
-        Thread offer = new Thread(() -> queue.offer(1));
-        poll.start();
-        offer.start();
-        poll.join();
-        offer.join();
-        assertThat(this.value, is(1));
+        Thread producer = new Thread(() -> queue.offer(1));
+        consumer.start();
+        producer.start();
+        consumer.join();
+        producer.join();
+        assertThat(value, is(1));
     }
+
+    /**
+     * Тест на изменение состояния потока если первым запустится производитель
+     */
+    @Test
+    public void whenProducerStartFirstThenConsumerDontWait() throws InterruptedException {
+        SimplyBlockingQuene<Integer> queue = new SimplyBlockingQuene<>();
+        Thread consumer = new Thread(() -> {
+            try {
+                value = queue.poll();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        Thread producer = new Thread(() -> queue.offer(1));
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
+        assertThat(value, is(1));
+    }
+
 }
