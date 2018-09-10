723ba1f7ffcf931381dc4b252b28738e8c05d6c6
posionmov <mailto:posionmov@gmail.com>
2018-09-10 17:29:46 +0300

    4.3. XML XSLT JDBC Оптимизация [#20459]

---
+++ chapter_junior_003_sql/src/test/java/ru/job4j/xml/ResultSummTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.xml;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Класс для тестирования работы парсера XML файлов
+ * @author Galanov Sergey
+ * @since 10.09.2018
+ * @version 1.0
+ */
+public class ResultSummTest {
+
+    /**
+     * Тест на проверку корректности подсчета
+     */
+    @Test
+    public void whenCreate10ItemsThenResultIs55() {
+        //final String dir = System.getProperty("user.dir");
+        ResultSumm res = null;
+        try {
+            res = new ResultSumm(10);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertThat(res.getResult(), is(55d));
+    }
+
+}
