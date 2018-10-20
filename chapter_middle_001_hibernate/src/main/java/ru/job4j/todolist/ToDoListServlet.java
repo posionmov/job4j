348511682a83b2948ab5f71dc81c5fa64ab61135
posionmov <mailto:posionmov@gmail.com>
2018-10-20 14:25:54 +0300

    2. Лямбды и шаблон wrapper.[#77649]

--- chapter_middle_001_hibernate/src/main/java/ru/job4j/todolist/ToDoListServlet.java
+++ chapter_middle_001_hibernate/src/main/java/ru/job4j/todolist/ToDoListServlet.java
@@ -2,6 +2,7 @@ package ru.job4j.todolist;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 import org.json.JSONObject;
@@ -13,12 +14,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * Сервлет получения и обновления информации из БД для программы todoList
  * @author Galanov Sergey
  * @since 20.10.2018
- * @version 1.1
+ * @version 1.2
  */
 public class ToDoListServlet extends HttpServlet {
 
@@ -32,14 +34,8 @@ public class ToDoListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setCharacterEncoding("utf-8");
-        SessionFactory factory = new Configuration().configure().buildSessionFactory();
-        Session session = factory.openSession();
-        session.beginTransaction();
-        List<ListModel> list = session.createQuery("from ListModel").list();
+        List<ListModel> list = this.tx(session -> session.createQuery("from ListModel").list());
         resp.getWriter().append(new JSONObject().put("items", list).toString()).flush();
-        session.getTransaction().commit();
-        session.close();
-        factory.close();
     }
 
     /**
@@ -51,23 +47,42 @@ public class ToDoListServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        SessionFactory factory = new Configuration().configure().buildSessionFactory();
-        Session session = factory.openSession();
-        session.beginTransaction();
         if (req.getParameter("operation").equals("change")) {
-            Query query = session.createQuery("update ListModel set done = :done where id = :id");
-            query.setParameter("done", Boolean.valueOf(req.getParameter("done")));
-            query.setParameter("id", Integer.valueOf(req.getParameter("id")));
-            query.executeUpdate();
+            this.tx(session -> {Query query = session.createQuery(
+                    "update ListModel set done = :done where id = :id");
+                    query.setParameter("done", Boolean.valueOf(req.getParameter("done")));
+                    query.setParameter("id", Integer.valueOf(req.getParameter("id")));
+                    query.executeUpdate();
+                    return null;});
         } else if (req.getParameter("operation").equals("add")) {
             ListModel listModel = new ListModel();
             listModel.setDesc(req.getParameter("description"));
             listModel.setCreateDate(new Timestamp(System.currentTimeMillis()));
             listModel.setDone(false);
-            session.save(listModel);
+            this.tx(session -> session.save(listModel));
         }
-        session.getTransaction().commit();
-        session.close();
-        factory.close();
     }
+
+    /**
+     * Метод, принимающий в себя лямбда выражение и производящий её выполненеи
+     * @param command
+     * @param <T>
+     * @return
+     */
+    private <T> T tx(final Function<Session, T> command) {
+        final SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        final Session session = factory.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            return command.apply(session);
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            tx.commit();
+            session.close();
+            factory.close();
+        }
+    }
+
 }
